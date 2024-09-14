43d0878ade816aaf9390d234702a8ccaf2fb9d6b
benyaeger <benyaeger13@gmail.com>
2024-09-14 12:49:23 +0300

    JSON Responses
    - Altered all functions to return JSON Formatted Responses
    - Added more tests for each function, testing different scenarios

--- applicationServer.py
+++ applicationServer.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, url_for, Flask
+from flask import request, redirect, url_for, Flask, jsonify
 import psycopg2
 from psycopg2 import errors
 from datetime import datetime
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # Enable CORS for all routes
 CORS(app)
 
-# We connect to DB
+# Connecting to Real DB
 conn = psycopg2.connect(
     dbname='postgres',
     user='postgres',
@@ -19,6 +19,15 @@ conn = psycopg2.connect(
     port='5432'
 )
 
+# Connecting to A DUMMY DB
+# conn = psycopg2.connect(
+#     dbname='postgres',
+#     user='postgres',
+#     password='213746837',
+#     host='localhost',
+#     port='5432'
+# )
+
 # We create a cursor to the connection
 cur = conn.cursor()
 
@@ -26,7 +35,7 @@ cur = conn.cursor()
 # We use the route() decorator to tell Flask what URL should trigger our function.
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return jsonify(message='Hello World!')
 
 
 @app.route('/user')
@@ -37,45 +46,32 @@ def get_user():
     # Querying the DB for users that meet the parameters
     cur.execute(f"SELECT * FROM users WHERE first_name LIKE '%{first_name}%' AND last_name LIKE '%{last_name}%'")
     users = cur.fetchall()
+
+    user_list = []
+    for user in users:
+        user_dict = {
+            'user_id': user[0],
+            'first_name': user[1],
+            'last_name': user[2],
+            'user_created_at': user[3],
+            'user_details_updated_at': user[4]
+        }
+        user_list.append(user_dict)
+
     # If no users found
-    if len(users) == 0:
-        return '404 User Not Found'
-    return users
+    if len(user_list) == 0:
+        return jsonify(error='User Not Found'), 404
+    return jsonify(user_list)
 
 
-@app.route('/add_user_to_chat', methods=["POST"])
-def add_user_to_chat(chat_id=None, added_user_id=None):
-    if chat_id is None and added_user_id is None:
-        # Get params from request
-        chat_id = request.args.get('chat_id', '')
-        added_user_id = request.args.get('added_user_id', '')
-
-    # Insert Query Init
-    new_participant_insert_query = """
-    INSERT INTO participants (chat_id, user_id) VALUES (%s, %s)
-    """
-    try:
-        # Execute Insert Query
-        cur.execute(new_participant_insert_query, (chat_id, added_user_id))
-        # Commit the changes to the database
-        conn.commit()
-        return f'User ID {added_user_id} added Successfully to Chat ID {chat_id}'
-    except errors.UniqueViolation:
-        # Handle unique constraint violation, like adding the same user to the same chat
-        # The rollback() function ensures the data transaction is rolled back, preventing partial data from being commited to the DB
-        conn.rollback()
-        return f'Error: User ID {added_user_id} is already added to Chat ID {chat_id}', 400
-    except Exception as e:
-        # Handle other potential exceptions
-        conn.rollback()
-        return f'An Error occurred: {str(e)}', 500
-
-
+# Tested
 @app.route('/newchat', methods=["POST"])
 def new_chat():
     # We get new chat parameters from request parameters
     owner_id = request.args.get('owner_id', '')
     chat_name = request.args.get('chat_name', '')
+    if owner_id == '' or chat_name == '' or not owner_id or not chat_name:
+        return jsonify(message=f'At least one of the parameters is empty or was not provided'), 400
 
     # Prepare the new chat INSERT query to the chats table
     new_chat_insert_query = """
@@ -87,55 +83,139 @@ def new_chat():
     chat_created_at = datetime.now()  # Or any specific timestamp
     chat_owner_id = owner_id
     chat_details_updated_at = datetime.now()  # This is optional; if not specified, it defaults to CURRENT_TIMESTAMP
+    try:
+        # Execute the query with the provided values
+        cur.execute(new_chat_insert_query, (chat_name, chat_created_at, chat_owner_id, chat_details_updated_at))
 
-    # Execute the query with the provided values
-    cur.execute(new_chat_insert_query, (chat_name, chat_created_at, chat_owner_id, chat_details_updated_at))
+        # Fetch the chat generated ID
+        new_chat_id = cur.fetchone()[0]
 
-    # Fetch the chat generated ID
-    new_chat_id = cur.fetchone()[0]
+        # Commit the changes to the database
+        conn.commit()
 
-    # Commit the changes to the database
-    conn.commit()
+        # We also need to add the owner as a chat participant
+        # We use previously implemented function
+        add_user_to_chat(new_chat_id, owner_id)
+        return jsonify(message='Chat Created Successfully, Owner added to chat participants'), 200
 
-    # We also need to add the owner as a chat participant
-    # We use previously implemented function
-    add_user_to_chat(new_chat_id, owner_id)
-    return 'Chat Created Successfully, Owner added to chat participants'
-
-
-@app.route('/get_chat_participants')
-def get_chat_participants():
-    # Get chat id
-    chat_id = request.args.get('chat_id', '')
-
-    cur.execute(f'SELECT * FROM participants JOIN users ON participants.user_id = users.user_id WHERE chat_id = {chat_id}')
-
-    chat_participants = cur.fetchall()
-
-    if chat_participants is not None:
-        return chat_participants
-    return 'No Chat Participants Found'
+    except Exception as e:
+        return jsonify(error=f'An Error occurred: {e}'), 500
 
 
-@app.route('/remove_user_from_chat', methods=["PUT"])
-def remove_user_from_chat():
-    # TODO implement this function
-    return None
+# Tested
+@app.route('/add_user_to_chat', methods=["POST"])
+def add_user_to_chat(chat_id=None, added_user_id=None):
+    if chat_id is None and added_user_id is None:
+        # Get params from request
+        chat_id = request.args.get('chat_id', '')
+        added_user_id = request.args.get('added_user_id', '')
+        if chat_id == '' or added_user_id == '' or not chat_id or not added_user_id:
+            return jsonify(message=f'At least one of the parameters is empty or was not provided'), 400
+
+    # Insert Query Init
+    new_participant_insert_query = """
+    INSERT INTO participants (chat_id, user_id) VALUES (%s, %s)
+    """
+    try:
+        # Execute Insert Query
+        cur.execute(new_participant_insert_query, (chat_id, added_user_id))
+        # Commit the changes to the database
+        conn.commit()
+        return jsonify(message=f'User ID {added_user_id} added Successfully to Chat ID {chat_id}'), 200
+    except errors.UniqueViolation:
+        # Handle unique constraint violation, like adding the same user to the same chat
+        # The rollback() function ensures the data transaction is rolled back, preventing partial data from being commited to the DB
+        conn.rollback()
+        return jsonify(message=f'Error: User ID {added_user_id} is already added to Chat ID {chat_id}'), 400
+    except Exception as e:
+        # Handle other potential exceptions
+        conn.rollback()
+        return jsonify(message=f'An Error occurred: {str(e)}'), 500
 
 
+# Tested
+@app.route('/remove_user_from_chat', methods=["DELETE"])
+def remove_user_from_chat(chat_id=None, user_id=None):
+    if chat_id is None and user_id is None:
+        chat_id = request.args.get("chat_id", "")
+        user_id = request.args.get("user_id", "")
+        if chat_id == '' or user_id == '' or not chat_id or not user_id:
+            return jsonify(message=f'At least one of the parameters is empty or was not provided'), 400
+
+    delete_query = "DELETE FROM participants WHERE user_id = %s AND chat_id = %s"
+    try:
+        # Execute Delete Query
+        cur.execute(delete_query, (user_id, chat_id))
+
+        # Check if any row was deleted
+        if cur.rowcount == 0:
+            return jsonify(message=f'User ID {user_id} is not a participant in Chat ID {chat_id}'), 400
+
+        # Commit the changes to the database
+        conn.commit()
+        return jsonify(message=f'User ID {user_id} removed Successfully from Chat ID {chat_id}'), 200
+    except Exception as e:
+        return jsonify(error=f'An Error occurred: {e}'), 500
+
+
+# Tested
 @app.route('/user_chats')
 def get_chats_of_user():
     # We get user id name from request parameters
     owner_id = request.args.get('owner_id', '')
 
+    if owner_id == '' or not owner_id:
+        return jsonify(message=f'At least one of the parameters is empty or was not provided'), 400
+
     # Querying the DB for chats that meet the parameters
     cur.execute(f"SELECT * FROM chats WHERE chat_owner_id = {owner_id}")
-    chats = cur.fetchall()
+    chats_query = cur.fetchall()
+
+    chats = []
+    for chat in chats_query:
+        chats_dict = {
+            "chat_id": chat[0],
+            "chat_name": chat[1],
+            "chat_created_at": chat[2],
+            "chat_owner_id": chat[3],
+            "chat_details_updated_at": chat[4],
+        }
+        chats.append(chats_dict)
 
     # If no chats found
-    if len(chats) == 0:
-        return '404 Chats Not Found'
-    return chats
+    if len(chats) != 0:
+        return jsonify(chats), 200
+    return jsonify(error='No Chats Found'), 404
+
+
+# Tested
+@app.route('/get_chat_participants')
+def get_chat_participants():
+    # Get chat id
+    chat_id = request.args.get('chat_id', '')
+    if chat_id == '' or not chat_id:
+        return jsonify(message=f'At least one of the parameters is empty or was not provided'), 400
+
+    cur.execute(
+        f'SELECT * FROM participants JOIN users ON participants.user_id = users.user_id WHERE chat_id = {chat_id}')
+
+    chat_participants_query = cur.fetchall()
+
+    chat_participants = []
+    for participant in chat_participants_query:
+        print(participant)
+        participant_dict = {
+            'user_id': participant[2],
+            'first_name': participant[3],
+            'last_name': participant[4],
+            'user_created_at': participant[5],
+            'user_details_updated_at': participant[6]
+        }
+        chat_participants.append(participant_dict)
+
+    if len(chat_participants) != 0:
+        return jsonify(chat_participants), 200
+    return jsonify(error='No Chat Participants Found'), 404
 
 
 if __name__ == '__main__':
--- serverTest.py
+++ serverTest.py
@@ -26,6 +26,7 @@ def client():
 # This function tests the new_chat function of the server
 # @pytest.mark.skip(reason="Temporarily skipping this test")
 def test_new_chat(client):
+    # Test 1 - Expected Result: Good Response #
     # Define the test parameters
     owner_id = 1
     chat_name = 'Trofoty Habat'
@@ -33,53 +34,133 @@ def test_new_chat(client):
     # Simulate a POST request to the '/newchat' endpoint
     response = client.post('/newchat', query_string={'owner_id': owner_id, 'chat_name': chat_name})
 
-    # Check the response data
-    # Check status code is OK
+    # Check status code
     assert response.status_code == 200
+
+    # Test 2 - Expected Result: Bad Request - Insufficient Parameters Provided #
+    # Simulate a POST request to the '/newchat' endpoint
+    response = client.post('/newchat', query_string={'chat_name': chat_name})
+
+    # Check status code
+    assert response.status_code == 400
 
 
 # This function tests the add_user_to_chat function
 # @pytest.mark.skip(reason="Temporarily skipping this test")
 def test_add_user_to_chat(client):
+    # Test 1 - Expected Result: Good Response #
     chat_id = 1
     added_user_id = 7
 
+    # Simulate a POST request to the '/add_user_to_chat' endpoint
     response = client.post('/add_user_to_chat', query_string={'chat_id': chat_id, 'added_user_id': added_user_id})
 
-    print(response.data)
-    if response.status_code == 200:
-        assert b'added Successfully to Chat ID' in response.data
-    else:
-        # Check status code is OK
-        assert b'Error' in response.data
+    # Check status code
+    assert response.status_code == 200
+
+    # Test 2 - Expected Result: Bad Request - Trying to add an existing user to a chat #
+    # Simulate a POST request to the '/add_user_to_chat' endpoint
+    response = client.post('/add_user_to_chat', query_string={'chat_id': chat_id, 'added_user_id': added_user_id})
+
+    # Check status code
+    assert response.status_code == 400
+
+    # Test 3 - Expected Result: Bad Request - Insufficient Parameters Provided #
+    # Simulate a POST request to the '/add_user_to_chat' endpoint
+    response = client.post('/add_user_to_chat', query_string={'added_user_id': added_user_id})
+
+    # Check status code
+    assert response.status_code == 400
+
+
+# This function tests the remove_user_from_chat function
+# @pytest.mark.skip(reason="Temporarily skipping this test")
+def test_remove_user_from_chat(client):
+    # Test 1 - Expected Result: Good Response #
+    chat_id = 1
+    user_id = 7
+
+    # Simulate a DELETE request to the '/add_user_to_chat' endpoint
+    response = client.delete('/remove_user_from_chat', query_string={'chat_id': chat_id, 'user_id': user_id})
+
+    # Check status code
+    assert response.status_code == 200
+
+    # Test 2 - Expected Result: Bad Request - Trying to remove a user that does not exist in the chat #
+    # Simulate a DELETE request to the '/add_user_to_chat' endpoint
+    response = client.delete('/remove_user_from_chat', query_string={'chat_id': chat_id, 'user_id': user_id})
+
+    # Check status code
+    assert response.status_code == 400
+
+    # Test 3 - Expected Result: Bad Request - Insufficient Parameters Provided #
+    # Simulate a DELETE request to the '/add_user_to_chat' endpoint
+    response = client.delete('/remove_user_from_chat', query_string={'chat_id': chat_id})
+
+    # Check status code
+    assert response.status_code == 400
 
 
 # This function tests the get_chats_of_user function
 # @pytest.mark.skip(reason="Temporarily skipping this test")
 def test_get_chats_of_user(client):
+    # Test 1 - Expected Result: Good Response #
     # Define the test parameters
     owner_id = 1
 
-    # Simulate a POST request to the '/user_chats' endpoint
+    # Simulate a GET request to the '/user_chats' endpoint
     response = client.get('/user_chats', query_string={'owner_id': owner_id})
 
-    # Check status code is OK
+    # Check status code
     assert response.status_code == 200
+
+    # Test 2 - Expected Result: No Chats Found #
+    # Define the test parameters
+    owner_id = 88
+
+    # Simulate a GET request to the '/user_chats' endpoint
+    response = client.get('/user_chats', query_string={'owner_id': owner_id})
+
+    # Check status code
+    assert response.status_code == 404
+
+    # Test 3 - Expected Result: Bad Request - Insufficient Parameters Provided #
+    # Simulate a GET request to the '/user_chats' endpoint
+    response = client.get('/user_chats')
+
+    # Check status code
+    assert response.status_code == 400
 
 
 # This function test the get_chat_participants function
 # @pytest.mark.skip(reason="Temporarily skipping this test")
 def test_get_chat_participants(client):
+    # Test 1 - Expected Result: Good Response #
     # Define the test parameters
     chat_id = 1
 
     # Simulate a POST request to the '/get_chat_participants' endpoint
     response = client.get('/get_chat_participants', query_string={'chat_id': chat_id})
 
-    print(response.data)
-
-    # Check status code is OK
+    # Check status code
     assert response.status_code == 200
+
+    # Test 2 - Expected Result: No Participants Found #
+    # Define the test parameters
+    chat_id = 88
+
+    # Simulate a POST request to the '/get_chat_participants' endpoint
+    response = client.get('/get_chat_participants', query_string={'chat_id': chat_id})
+
+    # Check status code
+    assert response.status_code == 404
+
+    # Test 3 - Expected Result: Bad Request - Insufficient Parameters Provided #
+    # Simulate a POST request to the '/get_chat_participants' endpoint
+    response = client.get('/get_chat_participants')
+
+    # Check status code
+    assert response.status_code == 400
 
 
 if __name__ == '__main__':
